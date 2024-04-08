e37f8bb127d722f52d9335356f637f4e7a13483b
ThinkyMiner <131332283+ThinkyMiner@users.noreply.github.com>
2024-04-08 09:23:40 +0530

    labtest

--- LAB 7/2201104_assignment4.c
+++ LAB 7/2201104_assignment4.c
@@ -1,169 +1,106 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #define MAX_PROCESSES 100
 #define MAX_RESOURCES 100
 
-bool is_safe(int num_processes, int num_resources, int *available, int allocation[MAX_PROCESSES][MAX_RESOURCES], int need[MAX_PROCESSES][MAX_RESOURCES]) {
-    int work[MAX_RESOURCES];
-    bool finish[MAX_PROCESSES] = {false};
-    int sequence[MAX_PROCESSES];
-    int count = 0;
+bool is_safe(int num_processes, int num_resources, int *available,
+             int allocation[MAX_PROCESSES][MAX_RESOURCES],
+             int need[MAX_PROCESSES][MAX_RESOURCES]) {
+  int work[MAX_RESOURCES];
+  bool finish[MAX_PROCESSES] = {false};
+  int sequence[MAX_PROCESSES];
+  int count = 0;
 
-    for (int i = 0; i < num_resources; i++) {
-        work[i] = available[i];
-    }
+  // Initialize work to available resources
+  for (int i = 0; i < num_resources; i++) {
+    work[i] = available[i];
+  }
 
-    while (true) {
-        bool found = false;
-        for (int i = 0; i < num_processes; i++) {
-            if (!finish[i]) {
-                bool can_allocate = true;
-                for (int j = 0; j < num_resources; j++) {
-                    if (need[i][j] > work[j]) {
-                        can_allocate = false;
-                        break;
-                    }
-                }
-                if (can_allocate) {
-                    for (int j = 0; j < num_resources; j++) {
-                        work[j] += allocation[i][j];
-                    }
-                    finish[i] = true;
-                    sequence[count++] = i;
-                    found = true;
-                }
-            }
-        }
-        if (!found) {
-            break;
-        }
-    }
-
+  // Safety algorithm
+  while (true) {
+    bool found = false;
     for (int i = 0; i < num_processes; i++) {
-        if (!finish[i]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-void find_deadlock_processes(int num_processes, int num_resources, int *resource_instances, int allocation[MAX_PROCESSES][MAX_RESOURCES], int request[MAX_PROCESSES][MAX_RESOURCES], int *deadlock_processes, int *num_deadlock_processes) {
-    int need[MAX_PROCESSES][MAX_RESOURCES];
-    int available[MAX_RESOURCES];
-
-    for (int i = 0; i < num_resources; i++) {
-        available[i] = resource_instances[i];
-    }
-
-    for (int i = 0; i < num_processes; i++) {
+      if (!finish[i]) {
+        bool can_allocate = true;
         for (int j = 0; j < num_resources; j++) {
-            need[i][j] = request[i][j] - allocation[i][j];
-            available[j] -= allocation[i][j];
+          if (need[i][j] > work[j]) {
+            can_allocate = false;
+            break;
+          }
         }
-    }
-
-    for (int num_terminate = 1; num_terminate <= num_processes; num_terminate++) {
-        int terminate_processes[MAX_PROCESSES];
-        for (int i = 0; i < num_processes; i++) {
-            int bit = 1;
-            for (int j = 0; j < num_terminate; j++) {
-                if ((i & bit) != 0) {
-                    terminate_processes[j] = i;
-                }
-                bit <<= 1;
-            }
-
-            int new_available[MAX_RESOURCES];
-            int new_allocation[MAX_PROCESSES][MAX_RESOURCES];
-            int new_need[MAX_PROCESSES][MAX_RESOURCES];
-
-            for (int j = 0; j < num_resources; j++) {
-                new_available[j] = available[j];
-            }
-
-            for (int j = 0; j < num_processes; j++) {
-                bool terminate = false;
-                for (int k = 0; k < num_terminate; k++) {
-                    if (terminate_processes[k] == j) {
-                        terminate = true;
-                        break;
-                    }
-                }
-                if (terminate) {
-                    for (int k = 0; k < num_resources; k++) {
-                        new_available[k] += allocation[j][k];
-                        new_allocation[j][k] = 0;
-                        new_need[j][k] = 0;
-                    }
-                } else {
-                    for (int k = 0; k < num_resources; k++) {
-                        new_allocation[j][k] = allocation[j][k];
-                        new_need[j][k] = need[j][k];
-                    }
-                }
-            }
-
-            if (is_safe(num_processes, num_resources, new_available, new_allocation, new_need)) {
-                *num_deadlock_processes = num_terminate;
-                for (int j = 0; j < num_terminate; j++) {
-                    deadlock_processes[j] = terminate_processes[j];
-                }
-                return;
-            }
+        if (can_allocate) {
+          for (int j = 0; j < num_resources; j++) {
+            work[j] += allocation[i][j];
+          }
+          finish[i] = true;
+          sequence[count++] = i;
+          found = true;
         }
+      }
     }
+    // If no process can be finished, break the loop
+    if (!found) {
+      break;
+    }
+  }
 
-    *num_deadlock_processes = 0;
+  // If all processes finish, the system is in a safe state
+  for (int i = 0; i < num_processes; i++) {
+    if (!finish[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 int main() {
-    int num_resources, num_processes;
-    int resource_instances[MAX_RESOURCES];
-    int allocation[MAX_PROCESSES][MAX_RESOURCES];
-    int request[MAX_PROCESSES][MAX_RESOURCES];
+  int num_resources, num_processes;
+  int resource_instances[MAX_RESOURCES];
+  int allocation[MAX_PROCESSES][MAX_RESOURCES];
+  int request[MAX_PROCESSES][MAX_RESOURCES];
 
-    printf("Enter the number of distinct resources: ");
-    scanf("%d", &num_resources);
+  printf("Enter the number of distinct resources: ");
+  scanf("%d", &num_resources);
 
-    printf("Enter the number of instances of each resource type: ");
-    for (int i = 0; i < num_resources; i++) {
-        scanf("%d", &resource_instances[i]);
+  printf("Enter the number of instances of each resource type: ");
+  for (int i = 0; i < num_resources; i++) {
+    scanf("%d", &resource_instances[i]);
+  }
+
+  printf("Enter the number of processes: ");
+  scanf("%d", &num_processes);
+
+  printf("Enter the allocation matrix:\n");
+  for (int i = 0; i < num_processes; i++) {
+    for (int j = 0; j < num_resources; j++) {
+      scanf("%d", &allocation[i][j]);
     }
+  }
 
-    printf("Enter the number of processes: ");
-    scanf("%d", &num_processes);
-
-    printf("Enter the allocation matrix:\n");
-    for (int i = 0; i < num_processes; i++) {
-        for (int j = 0; j < num_resources; j++) {
-            scanf("%d", &allocation[i][j]);
-        }
+  printf("Enter the request matrix:\n");
+  for (int i = 0; i < num_processes; i++) {
+    for (int j = 0; j < num_resources; j++) {
+      scanf("%d", &request[i][j]);
     }
+  }
 
-    printf("Enter the request matrix:\n");
-    for (int i = 0; i < num_processes; i++) {
-        for (int j = 0; j < num_resources; j++) {
-            scanf("%d", &request[i][j]);
-        }
+  // Calculate the need matrix
+  int need[MAX_PROCESSES][MAX_RESOURCES];
+  for (int i = 0; i < num_processes; i++) {
+    for (int j = 0; j < num_resources; j++) {
+      need[i][j] = request[i][j] - allocation[i][j];
     }
+  }
 
-    int deadlock_processes[MAX_PROCESSES];
-    int num_deadlock_processes;
+  // Check if the system is in a safe state
+  if (is_safe(num_processes, num_resources, resource_instances, allocation,
+              need)) {
+    printf("No deadlock.\n");
+  } else {
+    printf("Deadlock detected.\n");
+  }
 
-    find_deadlock_processes(num_processes, num_resources, resource_instances, allocation, request, deadlock_processes, &num_deadlock_processes);
-
-    if (num_deadlock_processes == 0) {
-        printf("No deadlock.\n");
-    } else {
-        printf("Deadlock exists.\n");
-        printf("The smallest set of processes whose termination ends the deadlock: ");
-        for (int i = 0; i < num_deadlock_processes; i++) {
-            printf("%d ", deadlock_processes[i]);
-        }
-        printf("\n");
-    }
-
-    return 0;
+  return 0;
 }
--- LAB 7/main.c
+++ LAB 7/main.c
@@ -1,29 +1,135 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdbool.h>
+#include <stdio.h>
 
-bool isSafe(int numProcesses, int numResources, int available[], int max[][numResources], int allocation[][numResources], int process){
-    
-    int need[numResources][numProcesses];
+#define MAX_PROCESSES 10
+#define MAX_RESOURCES 10
 
-    for(int i = 0 ; i < numResources; i++){
-        for(int j = 0; j < numProcesses; j++){
-            need[i][j] = max[i][j] - allocation[i][j];
-        }
+typedef struct {
+  int name;
+  int allocated[MAX_RESOURCES];
+  int requested[MAX_RESOURCES];
+} Process;
+
+typedef struct {
+  int name;
+} Resource;
+
+bool isSafe(Process processes[], int n, Resource resources[], int m,
+            int excluded[]) {
+  int work[MAX_RESOURCES];
+  for (int i = 0; i < m; i++) {
+    work[i] = resources[i].name; // Initial number of available resources
+  }
+
+  bool finish[MAX_PROCESSES] = {false};
+
+  for (int i = 0; i < n; i++) {
+    if (excluded[i] == 1 || finish[i] == true) {
+      continue; // Skip excluded or finished processes
     }
-    
-    for (int i = 0; i < numResources; i++){
-        for(int j = 0 ; j < numProcesses; j++){
-            if(need[i][j] < available[i]){
-                return false;
-            }
-        }
+    bool canFinish = true;
+    for (int j = 0; j < m; j++) {
+      if (processes[i].requested[j] > work[j]) {
+        canFinish = false;
+        break;
+      }
     }
-    return true;
+    if (canFinish) {
+      for (int j = 0; j < m; j++) {
+        work[j] += processes[i].allocated[j];
+      }
+      finish[i] = true;
+    }
+  }
+
+  // Check if all processes are finished
+  for (int i = 0; i < n; i++) {
+    if (excluded[i] == 0 && finish[i] == false) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-int main(int argc, char const *argv[])
-{
-    
-    return 0;
+void findSmallestDeadlockFreeSet(Process processes[], int n,
+                                 Resource resources[], int m,
+                                 int currentExcluded[], int size, int *minSize,
+                                 int *minSet) {
+  if (size >= n) {
+    return; // Reached end of recursion
+  }
+
+  // Check if current excluded set avoids deadlock
+  if (isSafe(processes, n, resources, m, currentExcluded)) {
+    if (size < *minSize) {
+      *minSize = size;
+      for (int i = 0; i < n; i++) {
+        minSet[i] = currentExcluded[i];
+      }
+    }
+    return; // No need to explore further if smaller set found
+  }
+
+  // Try excluding each remaining process
+  for (int i = 0; i < n; i++) {
+    if (currentExcluded[i] == 0) {
+      currentExcluded[i] = 1;
+      findSmallestDeadlockFreeSet(processes, n, resources, m, currentExcluded,
+                                  size + 1, minSize, minSet);
+      currentExcluded[i] = 0; // Backtrack
+    }
+  }
+}
+
+int main() {
+  int n, m; // Number of processes and resources
+
+  printf("Enter the number of processes: ");
+  scanf("%d", &n);
+
+  printf("Enter the number of resources: ");
+  scanf("%d", &m);
+
+  Process processes[MAX_PROCESSES];
+  Resource resources[MAX_RESOURCES];
+
+  printf("Enter details of processes:\n");
+  for (int i = 0; i < n; i++) {
+    processes[i].name = i;
+    printf("Process %d:\n", i);
+    printf("Allocated resources (space-separated): ");
+    for (int j = 0; j < m; j++) {
+      scanf("%d", &processes[i].allocated[j]);
+    }
+    printf("Requested resources (space-separated): ");
+    for (int j = 0; j < m; j++) {
+      scanf("%d", &processes[i].requested[j]);
+    }
+  }
+
+  printf("Enter details of resources:\n");
+  for (int i = 0; i < m; i++) {
+    resources[i].name = i;
+    printf("Resource %d: ", i);
+    scanf("%d", &resources[i].name);
+  }
+
+  int minSize = n;
+  int minSet[MAX_PROCESSES] = {
+      0}; // To store process indices in the smallest set
+  int excluded[MAX_PROCESSES] = {0}; // Initialize excluded array with zeros
+
+  findSmallestDeadlockFreeSet(processes, n, resources, m, excluded, 0, &minSize,
+                              minSet);
+
+  printf("Smallest deadlock-free set of processes: ");
+  for (int i = 0; i < n; i++) {
+    if (minSet[i] == 1) {
+      printf("%d ", i);
+    }
+  }
+  printf("\n");
+
+  return 0;
 }
